0112cea112288a2ad34b927754a43245dc039f59
guigajks <guigajks@gmail.com>
2022-12-13 23:13:01 +0000

    responsive nav

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function openEmailClient() {
 
     anchor.click()
 }
+
+(() => {
+    const navButton = document.getElementById('navButton')
+    const nav = navButton.parentElement
+    const navList = nav.querySelector('ul')
+
+    navButton.addEventListener('click', () => {
+        if (navList.getAttribute('open') === 'true') {
+            navList.setAttribute('open', 'false')
+            nav.setAttribute('open', 'false')
+        } else {
+            navList.setAttribute('open', 'true')
+            nav.setAttribute('open', 'true')
+        }
+    })
+})()
